f43db3026f3e030b858089668e8f8b97a73dff77
babylee2002 <babylee2002@gmail.com>
2020-08-31 23:27:52 -0400

    fixed

--- src/components/Particles.js
+++ src/components/Particles.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Particles from 'react-particles-js';
-
+import ReactTooltip from 'react-tooltip';
+console.log('window.location.pathname',window.location)
 const Particle = () => {
     return (
        <div 
+       data-tip={window.location.pathname!=='/apps' ? 'click to create bubbles!' : null}
        data-type='light'
        data-class = 'tooltip-transparent'
        >
@@ -126,6 +128,7 @@ const Particle = () => {
                 }
             }
             } />
+            <ReactTooltip className='tooltip' />
             </div> 
     )
 }
