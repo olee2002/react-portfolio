87feb90aa06dc1eb03e230c0c4353ed252b3f469
babylee2002 <babylee2002@gmail.com>
2020-09-08 22:53:17 -0400

    change submit to register

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -107,7 +107,7 @@ class SignUpForm extends Component {
                      <Link to='login'>
                         <button>LogIn</button>
                      </Link>
-                     <button onClick={this.handleSubmit}>Submit</button>
+                     <button onClick={this.handleSubmit}>Register</button>
                   </div>
                </form>
             ) : (
