f660a451185662e0c598b4d9c76952436768b09b
babylee2002 <babylee2002@gmail.com>
2020-09-08 22:49:23 -0400

    update signupform

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,114 +1,137 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import { connect } from 'react-redux';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import { connect } from 'react-redux'
 
-import { registerUsers } from '../redux/actions/signupActionCreator';
+import { registerUsers } from '../redux/actions/signupActionCreator'
 
 class SignUpForm extends Component {
+   state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      passwordShown: false,
+      isRegistered: false,
+      errmsg: '',
+      user: {},
+   }
 
-    state = {
-        firstName: '',
-        lastName: '',
-        email: '',
-        password: '',
-        passwordShown: false,
-        isRegistered: false,
-        errmsg: '',
-        user: {}
-    }
+   componentWillReceiveProps = (nextProps) => {
+      const { signup } = nextProps.state
+      this.setState({
+         user: signup ? signup.user : {},
+         errmsg: signup ? signup.errmsg : '',
+         fetched: signup ? signup.fetched : false,
+      })
+   }
 
-    componentWillReceiveProps = (nextProps) => {
-        const { signup } = nextProps.state;
-        this.setState({
-            user: signup ? signup.user : {},
-            errmsg: signup ? signup.errmsg : '',
-            fetched: signup ? signup.fetched : false
-        })
-    }
+   handleChange = (name) => {
+      return (e) => {
+         this.setState({
+            [name]: e.target.value,
+            errmsg: '',
+         })
+      }
+   }
 
-    handleChange = name => {
-        return (e) => {
-            this.setState({
-                [name]: e.target.value, errmsg: ''
-            });
-        }
-    }
+   handleSubmit = (e) => {
+      const { firstName, lastName, email, password, passwordShown } = this.state
+      e.preventDefault()
+      const payload = { firstName, lastName, email, password }
+      if (firstName && lastName && email && password) {
+         this.props.registerUsers(payload)
+      } else {
+         this.setState({ errmsg: 'All Fields Required!' })
+      }
+   }
+   handlePassword = (e) => {
+      e.preventDefault()
+      this.setState({ passwordShown: !this.state.passwordShown })
+   }
 
-    handleSubmit = (e) => {
-        const { firstName, lastName, email, password, passwordShown } = this.state;
-        e.preventDefault();
-        const payload = { firstName, lastName, email, password }
-        if (firstName && lastName && email && password) {
-            this.props.registerUsers(payload);
-        } else {
-            this.setState({ errmsg: 'All Fields Required!' });
-        }
-    }
-    handlePassword = (e) => {
-        e.preventDefault();
-        this.setState({ passwordShown: !this.state.passwordShown })
-    }
-
-    render() {
-        const user = sessionStorage.getItem("user");
-        const { passwordShown } = this.state;
-        const { errmsg } = this.state;
-        return (
-            <Container>
-                {!this.state.isRegistered && !user ?
-
-                    <form>
-                        <h3>Please Sign Up!</h3>
-                        <div style={{ fontSize: '12px', color: 'red' }}>{errmsg ? errmsg : null}</div>
-                        <div>
-                            <label>Firstname</label>
-                            <input type='text' onChange={this.handleChange('firstName')} />
-                        </div>
-                        <div>
-                            <label>Lastname</label>
-                            <input type='text' onChange={this.handleChange('lastName')} />
-                        </div>
-                        <div>
-                            <label>Email</label>
-                            <input type='text' onChange={this.handleChange('email')} />
-                        </div>
-                        <div>
-                            <label>Password</label>
-                            <input
-                                required={true}
-                                type={!passwordShown ? 'password' : 'text'}
-                                onChange={this.handleChange('password')} />
-                        </div>
-                        <div>
-                            <button onClick={this.handlePassword}>
-                                {passwordShown ? 'hide password' : 'show password'}
-                            </button>
-                            <button onClick={this.handleSubmit}>
-                                Submit
-                            </button>
-                        </div>
-                    </form>
-                    :
-                    <Message>You are successfully Registered! Please log in to proceed!</Message>}
-            </Container>
-        )
-    }
+   render() {
+      const user = sessionStorage.getItem('user')
+      const { passwordShown } = this.state
+      const { errmsg } = this.state
+      return (
+         <Container>
+            {!this.state.isRegistered && !user ? (
+               <form>
+                  <h3>Please Sign Up!</h3>
+                  <div style={{ fontSize: '12px', color: 'red' }}>
+                     {errmsg ? errmsg : null}
+                  </div>
+                  <div>
+                     <label>Firstname</label>
+                     <input
+                        type='text'
+                        onChange={this.handleChange('firstName')}
+                     />
+                  </div>
+                  <div>
+                     <label>Lastname</label>
+                     <input
+                        type='text'
+                        onChange={this.handleChange('lastName')}
+                     />
+                  </div>
+                  <div>
+                     <label>Email</label>
+                     <input type='text' onChange={this.handleChange('email')} />
+                  </div>
+                  <div>
+                     <label>Password</label>
+                     <input
+                        required={true}
+                        type={!passwordShown ? 'password' : 'text'}
+                        onChange={this.handleChange('password')}
+                     />
+                     <small
+                        onClick={this.handlePassword}
+                        style={{
+                           position: 'absolute',
+                           marginLeft: -30,
+                           marginTop: 8,
+                           cursor: 'pointer',
+                        }}>
+                        {' '}
+                        {!this.state.passwordShown ? (
+                           <i class='fa fa-eye' aria-hidden='true'></i>
+                        ) : (
+                           <i class='fa fa-eye-slash' aria-hidden='true'></i>
+                        )}
+                     </small>
+                  </div>
+                  <div>
+                     <Link to='login'>
+                        <button>LogIn</button>
+                     </Link>
+                     <button onClick={this.handleSubmit}>Submit</button>
+                  </div>
+               </form>
+            ) : (
+               <Message>
+                  You are successfully Registered! Please log in to proceed!
+               </Message>
+            )}
+         </Container>
+      )
+   }
 }
 
-const mapStateToProps = state => ({
-    state: state,
-    user: state.user,
-    errmsg: state.errmsg,
-    fetched: state.feched
+const mapStateToProps = (state) => ({
+   state: state,
+   user: state.user,
+   errmsg: state.errmsg,
+   fetched: state.feched,
+})
 
-});
+const mapDispatchToProps = (dispatch) => ({
+   registerUsers: (payload) => dispatch(registerUsers(payload)),
+})
 
-const mapDispatchToProps = dispatch => ({
-    registerUsers: (payload) => dispatch(registerUsers(payload))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(SignUpForm);
-
+export default connect(mapStateToProps, mapDispatchToProps)(SignUpForm)
 
 const Container = styled.div`
 width: 100vw;
@@ -161,12 +184,11 @@ button{
         width: 97.5px; 
         font-size: 10px;
     }
-`;
-
+`
 
 const Message = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    margin-right: 20px;
-`;
+   display: flex;
+   flex-direction: row;
+   justify-content: flex-start;
+   margin-right: 20px;
+`
